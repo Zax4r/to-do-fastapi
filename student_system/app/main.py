50f85705f1dc769af20616ecc6be4fbfe633590f
Zax4r <zaharbedarik@gmail.com>
2026-02-25 15:07:34 +0300

    deleted json and logging

--- student_system/app/main.py
+++ student_system/app/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 import os
-import logging
 from app.students.router import router_students
 from app.majors.router import router_majors
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(script_dir)
-path_to_json = os.path.join(parent_dir,'students.json')
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    db_url = os.getenv("DATABASE_URL")
+    print(f"DATABASE_URL from env: {db_url}")
 
 app.include_router(router_students)
 app.include_router(router_majors)
