53b78177350a1eb14232f2a6ff3cd651a037cb37
Zax4r <zaharbedarik@gmail.com>
2026-03-02 08:52:53 +0300

    register endpoint

--- to_do/app/routers/auth.py
+++ to_do/app/routers/auth.py
@@ -2,15 +2,24 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response
 from app.models.dependecies import DbDep
 from app.core.jwt import create_access_token
 from app.core.config import get_jwt_info
-from app.core.security import authenticate_user
-from app.schemas.registrations import SRegister
+from app.core.security import authenticate_user, hash_password
+from app.schemas.registrations import SRegister, SLogin
+from app.services.users import UserService
 
 
 router = APIRouter(prefix='/registration',tags=['Управление регистрацией'])
 
+@router.post('/register/')
+async def add_user(user: SRegister, session: DbDep):
+    new_user_dict = user.model_dump()
+    new_user_dict['password'] = hash_password(new_user_dict['password'])
+    check = await UserService.add_one(session,**new_user_dict)
+    if check:
+        return {'message':f'Пользователь добавлен:{user}'}
+    return {'message':f'Ошибка добавления пользователя:{user}'}
 
 @router.post('/login/')
-async def login(session: DbDep, response: Response, form: SRegister = Depends()):
+async def login(session: DbDep, response: Response, form: SLogin = Depends()):
     user = await authenticate_user(session, form.email, form.password)
     if not user:
         raise HTTPException(
