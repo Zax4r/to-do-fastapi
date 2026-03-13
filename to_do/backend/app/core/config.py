19acf1576bbdf6d1715adde9a967396acffb9e57
Zax4r <zaharbedarik@gmail.com>
2026-03-13 11:26:06 +0300

    added redis url

--- to_do/backend/app/core/config.py
+++ to_do/backend/app/core/config.py
@@ -3,13 +3,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:1111@pg:5432/to_do_db")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    REDIS_URL: str =  os.getenv("REDIS_URL")
 
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "dev-secret")
-    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
-    CORS_ORIGINS: list = eval(os.getenv("CORS_ORIGINS", '["http://localhost:8051"]'))
+    CORS_ORIGINS: list = eval(os.getenv("CORS_ORIGINS"))
 
 
 settings = Settings()
@@ -20,3 +22,6 @@ def get_db_url():
 
 def get_jwt_info():
     return {'SECRET_KEY':settings.SECRET_KEY,'ALGORITHM':settings.ALGORITHM,'ACCESS_TOKEN_EXPIRE_MINUTES':settings.ACCESS_TOKEN_EXPIRE_MINUTES}
+
+def get_redis_url():
+    return settings.REDIS_URL
